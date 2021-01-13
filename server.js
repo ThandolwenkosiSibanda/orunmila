0b837dcf93e4693054641c26a6f99c355780351b
unknown <sibandathandolwenkosi2@gmail.com>
2021-01-13 17:33:42 +0200

    commit test

--- server.js
+++ server.js
@@ -278,8 +278,8 @@ app.use(function(req, res, next) {
 // set up cors to allow us to accept requests from our client
 app.use(
 	cors({
-		// origin      : 'http://kratos-dev.herokuapp.com',
-		origin      : 'http://localhost:3000', // allow to server to accept request from different origin
+		origin      : 'http://kratos-dev.herokuapp.com',
+		// origin      : 'http://localhost:3000', // allow to server to accept request from different origin
 		methods     : 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials : true // allow session cookie from browser to pass through
 	})
